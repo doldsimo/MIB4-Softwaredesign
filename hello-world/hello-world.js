16bfffcb124977418adf53c9058ef20dac870d65
Simon Pirmann <s.pirmann@sdv-werbestudio.de>
2020-04-28 10:08:48 +0200

    new build over vsc

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -1,13 +1,14 @@
-var HelloWorld = /** @class */ (function () {
-    function HelloWorld(greeting, name) {
+"use strict";
+class HelloWorld {
+    constructor(greeting, name) {
         this.id = Math.floor(Math.random() * 100) + 1;
         this.greeting = greeting;
         this.name = name;
     }
-    HelloWorld.prototype.sayHello = function () {
+    sayHello() {
         console.log(this.greeting + ' ' + this.name + '! Your personal ID is: ' + this.id);
-    };
-    return HelloWorld;
-}());
+    }
+}
 var hello = new HelloWorld('Welcome', 'Simon');
 hello.sayHello();
+//# sourceMappingURL=hello-world.js.map
