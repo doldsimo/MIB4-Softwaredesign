e547a143b09def4917453c7bb4bb5c6429e9a554
Simon Pirmann <s.pirmann@sdv-werbestudio.de>
2020-04-29 12:21:25 +0200

    updated own concept, coded maurice concept

---
+++ L01/main.js
@@ -0,0 +1,37 @@
+"use strict";
+var L01_Worteverdreher;
+(function (L01_Worteverdreher) {
+    let eingabe_satz;
+    eingabe_satz = prompt('Gebe einen Satz ein.');
+    sentence_wr(eingabe_satz);
+    sentence_sr(eingabe_satz);
+    sentence_ar(eingabe_satz);
+    function sentence_wr(eingabe_satz) {
+        console.log(reverseWords(reverseSentence(eingabe_satz)));
+    }
+    function sentence_sr(eingabe_satz) {
+        console.log(reverseWords(eingabe_satz));
+    }
+    function sentence_ar(eingabe_satz) {
+        console.log(reverseSentence(eingabe_satz));
+    }
+    function reverseSentence(eingabe_satz) {
+        let zeichen;
+        let rev_zeichen;
+        zeichen = eingabe_satz.split('');
+        for (let i = 0; i < zeichen.length; i++) {
+            rev_zeichen += zeichen[zeichen.length - i];
+        }
+        return rev_zeichen;
+    }
+    function reverseWords(eingabe_satz) {
+        let woerter;
+        let rev_woerter;
+        woerter = eingabe_satz.split(' ');
+        for (let i = 0; i < woerter.length; i++) {
+            rev_woerter += woerter[woerter.length - i] + ' ';
+        }
+        return rev_woerter;
+    }
+})(L01_Worteverdreher || (L01_Worteverdreher = {}));
+//# sourceMappingURL=main.js.map
---
+++ L01/main.ts
@@ -0,0 +1,45 @@
+namespace L01_Worteverdreher {
+	let eingabe_satz: string;
+	eingabe_satz = prompt('Gebe einen Satz ein.');
+	sentence_wr(eingabe_satz);
+	sentence_sr(eingabe_satz);
+	sentence_ar(eingabe_satz);
+
+	function sentence_wr(eingabe_satz: string): void {
+		console.log(reverseWords(reverseSentence(eingabe_satz)));
+	}
+
+	function sentence_sr(eingabe_satz: string): void {
+		console.log(reverseWords(eingabe_satz));
+	}
+
+	function sentence_ar(eingabe_satz: string): void {
+		console.log(reverseSentence(eingabe_satz));
+	}
+
+	function reverseSentence(eingabe_satz: string): string {
+		let zeichen: string[];
+		let rev_zeichen: string;
+
+		zeichen = eingabe_satz.split('');
+
+		for (let i: number = 0; i < zeichen.length; i++) {
+			rev_zeichen += zeichen[zeichen.length - i];
+		}
+
+		return rev_zeichen;
+	}
+
+	function reverseWords(eingabe_satz: string): string {
+		let woerter: string[];
+		let rev_woerter: string;
+
+		woerter = eingabe_satz.split(' ');
+
+		for (let i: number = 0; i < woerter.length; i++) {
+			rev_woerter += woerter[woerter.length - i] + ' ';
+		}
+
+		return rev_woerter;
+	}
+}
