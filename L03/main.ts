7bafeafc492947ca2c076e4db6f3c521c3c6a95a
Simon Pirmann <32400302+simonpirmann@users.noreply.github.com>
2020-05-13 10:35:36 +0200

    Änderungen durchgeführt

--- L03/main.ts
+++ L03/main.ts
@@ -3,10 +3,9 @@ namespace L03_Zufallsgedicht {
 	let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 	let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren"];
 	let verses: string[] = [];
-	// Z: 7 -> Variablen sollten camelCase sein
-	let subject_length: number = subjects.length;
+	let subjectLength: number = subjects.length;
 
-	for (let i: number = 0; i < subject_length; i++) {
+	for (let i: number = 0; i < subjectLength; i++) {
 		getVers();
 	}
 
@@ -15,20 +14,18 @@ namespace L03_Zufallsgedicht {
 	}
 
 	function getVers(): void {
-		// Z: 19-28 -> Variablen sollten camelCase sein
-		let random_subject_number: number = Math.floor(Math.random() * subject_length);
-		let random_subject: string = subjects[random_subject_number];
-		subjects.splice(random_subject_number, 1);
+		let randomSubjectNumber: number = Math.floor(Math.random() * subjectLength);
+		let randomSubject: string = subjects[randomSubjectNumber];
+		subjects.splice(randomSubjectNumber, 1);
 
-		let random_verb_number: number = Math.floor(Math.random() * verbs.length);
-		let random_verb: string = verbs[random_verb_number];
-		verbs.splice(random_verb_number, 1);
+		let randomVerbNumber: number = Math.floor(Math.random() * verbs.length);
+		let randomVerb: string = verbs[randomVerbNumber];
+		verbs.splice(randomVerbNumber, 1);
 
-		let random_object_number: number = Math.floor(Math.random() * objects.length);
-		let random_object: string = objects[random_object_number];
-		objects.splice(random_object_number, 1);
-		// Z: 30 -> Es sollten doppelte Anfuehrungszeichen genutzt werden
-		let verse: string = random_subject + ' ' + random_verb + ' ' + random_object;
+		let randomObjectNumber: number = Math.floor(Math.random() * objects.length);
+		let randomObject: string = objects[randomObjectNumber];
+		objects.splice(randomObjectNumber, 1);
+		let verse: string = randomSubject + ' ' + randomVerb + ' ' + randomObject;
 
 		verses.push(verse);
 	}
