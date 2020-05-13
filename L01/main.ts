8dc00e255bca61d8908d31ddf6e702d88a0d7b29
doldsimo <48808400+doldsimo@users.noreply.github.com>
2020-05-13 10:58:31 +0200

    Code Review L01
    Code Review von Simon Dold

--- L01/main.ts
+++ L01/main.ts
@@ -1,10 +1,12 @@
 namespace L01_Worteverdreher {
+	// Z: 3 -> Variable in camelCase
 	let eingabe_satz: string;
+	// Z: 4 -> Doppelte Anfuehurngszeichen
 	eingabe_satz = prompt('Gebe einen Satz ein.');
 	sentence_wr(eingabe_satz);
 	sentence_sr(eingabe_satz);
 	sentence_ar(eingabe_satz);
-
+	// Z: 10-18 -> Funktionen in camelCase schreiben und uebergebene Parameter mit underscore am Anfang (z.B: _name)
 	function sentence_wr(eingabe_satz: string): void {
 		console.log(reverseWords(reverseSentence(eingabe_satz)));
 	}
@@ -16,9 +18,10 @@ namespace L01_Worteverdreher {
 	function sentence_ar(eingabe_satz: string): void {
 		console.log(reverseSentence(eingabe_satz));
 	}
-
+	// Z: 22 -> Uebergebene Parameter mit underscore am Anfang (z.B: _name)
 	function reverseSentence(eingabe_satz: string): string {
 		let zeichen: string[];
+		// Z:25-27 -> Doppelte Anfuehrungszeichen
 		let rev_zeichen: string = '';
 
 		zeichen = eingabe_satz.split('');
@@ -29,14 +32,16 @@ namespace L01_Worteverdreher {
 
 		return rev_zeichen;
 	}
-
+	// Z: 36 -> Uebergebene Parameter mit underscore am Anfang (z.B: _name)
 	function reverseWords(eingabe_satz: string): string {
 		let woerter: string[];
+		// Z:39-41 -> Doppelte Anfuehrungszeichen
 		let rev_woerter: string = '';
 
 		woerter = eingabe_satz.split(' ');
 
 		for (let i: number = 1; i <= woerter.length; i++) {
+			// Z:45 -> Doppelte Anfuehrungszeichen
 			rev_woerter += woerter[woerter.length - i] + ' ';
 		}
 
