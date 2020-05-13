cda6e318fb8b9bf6d4f061625cade8aa8d618ff5
doldsimo <48808400+doldsimo@users.noreply.github.com>
2020-05-13 10:28:08 +0200

    Code Review
    Code Review von Simon Dold

--- L03/main.ts
+++ L03/main.ts
@@ -3,7 +3,7 @@ namespace L03_Zufallsgedicht {
 	let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 	let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren"];
 	let verses: string[] = [];
-
+	// Z: 7 -> Variablen sollten camelCase sein
 	let subject_length: number = subjects.length;
 
 	for (let i: number = 0; i < subject_length; i++) {
@@ -15,6 +15,7 @@ namespace L03_Zufallsgedicht {
 	}
 
 	function getVers(): void {
+		// Z: 19-28 -> Variablen sollten camelCase sein
 		let random_subject_number: number = Math.floor(Math.random() * subject_length);
 		let random_subject: string = subjects[random_subject_number];
 		subjects.splice(random_subject_number, 1);
@@ -26,7 +27,7 @@ namespace L03_Zufallsgedicht {
 		let random_object_number: number = Math.floor(Math.random() * objects.length);
 		let random_object: string = objects[random_object_number];
 		objects.splice(random_object_number, 1);
-
+		// Z: 30 -> Es sollten doppelte Anfuehrungszeichen genutzt werden
 		let verse: string = random_subject + ' ' + random_verb + ' ' + random_object;
 
 		verses.push(verse);
