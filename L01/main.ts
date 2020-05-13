e3e2cd6d762bd55402e1e9c464c8c14a3ed48d30
Simon Pirmann <32400302+simonpirmann@users.noreply.github.com>
2020-05-13 11:13:20 +0200

    Änderungen durchgeführt

--- L01/main.ts
+++ L01/main.ts
@@ -1,30 +1,27 @@
 namespace L01_Worteverdreher {
-	// Z: 3 -> Variable in camelCase
-	let eingabe_satz: string;
-	// Z: 4 -> Doppelte Anfuehurngszeichen
-	eingabe_satz = prompt('Gebe einen Satz ein.');
-	sentence_wr(eingabe_satz);
-	sentence_sr(eingabe_satz);
-	sentence_ar(eingabe_satz);
-	// Z: 10-18 -> Funktionen in camelCase schreiben und uebergebene Parameter mit underscore am Anfang (z.B: _name)
-	function sentence_wr(eingabe_satz: string): void {
-		console.log(reverseWords(reverseSentence(eingabe_satz)));
+	let eingabeSatz: string;
+	eingabeSatz = prompt("Gebe einen Satz ein.");
+	sentenceWR(eingabeSatz);
+	sentenceSR(eingabeSatz);
+	sentenceAR(eingabeSatz);
+	
+	function sentenceWR(_eingabeSatz: string): void {
+		console.log(reverseWords(reverseSentence(_eingabeSatz)));
 	}
 
-	function sentence_sr(eingabe_satz: string): void {
-		console.log(reverseWords(eingabe_satz));
+	function sentenceSR(_eingabeSatz: string): void {
+		console.log(reverseWords(_eingabeSatz));
 	}
 
-	function sentence_ar(eingabe_satz: string): void {
-		console.log(reverseSentence(eingabe_satz));
+	function sentenceAR(_eingabeSatz: string): void {
+		console.log(reverseSentence(_eingabeSatz));
 	}
-	// Z: 22 -> Uebergebene Parameter mit underscore am Anfang (z.B: _name)
-	function reverseSentence(eingabe_satz: string): string {
+	
+	function reverseSentence(_eingabeSatz: string): string {
 		let zeichen: string[];
-		// Z:25-27 -> Doppelte Anfuehrungszeichen
-		let rev_zeichen: string = '';
+		let rev_zeichen: string = "";
 
-		zeichen = eingabe_satz.split('');
+		zeichen = _eingabeSatz.split("");
 
 		for (let i: number = 1; i <= zeichen.length; i++) {
 			rev_zeichen += zeichen[zeichen.length - i];
@@ -32,17 +29,15 @@ namespace L01_Worteverdreher {
 
 		return rev_zeichen;
 	}
-	// Z: 36 -> Uebergebene Parameter mit underscore am Anfang (z.B: _name)
-	function reverseWords(eingabe_satz: string): string {
+	
+	function reverseWords(_eingabeSatz: string): string {
 		let woerter: string[];
-		// Z:39-41 -> Doppelte Anfuehrungszeichen
-		let rev_woerter: string = '';
+		let rev_woerter: string = "";
 
-		woerter = eingabe_satz.split(' ');
+		woerter = _eingabeSatz.split(" ");
 
 		for (let i: number = 1; i <= woerter.length; i++) {
-			// Z:45 -> Doppelte Anfuehrungszeichen
-			rev_woerter += woerter[woerter.length - i] + ' ';
+			rev_woerter += woerter[woerter.length - i] + " ";
 		}
 
 		return rev_woerter;
